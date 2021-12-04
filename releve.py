2c74482564c021dbd66fd9e4275d7f6c5194de14
Ertinox-08 <80890993+Ertinox-08@users.noreply.github.com>
2021-12-04 17:20:09 +0100

    Update files

--- releve.py
+++ releve.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
+#   Fichier avec données sur les relevès pluviométriques
 src = "data/input/releve.csv"
 
+#   Lecture de fichier "releve.csv"
 data = pd.read_csv(src)
 
+#   La somme des données 'data'
 somme = pd.DataFrame.sum(data)
 
-# print(somme)
-
+#   Dossier sortie des données récuperer dans "input"
 src_ouptut = "data/output/releve.txt"
+
+#   Ouverture du fichier de sortie et écriture de la somme des colonnes
 with open(src_ouptut, 'w') as f:
     f.write(str(somme))
