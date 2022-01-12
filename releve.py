5d689fc85f1e2eb3c1f48b491a8b93748901fc4c
Ertinox-08 <80890993+Ertinox-08@users.noreply.github.com>
2022-01-12 16:31:09 +0100

    Update

--- releve.py
+++ releve.py
@@ -12,4 +12,10 @@ for annee in YEAR:
     data = pd.read_csv(src)
     df = pd.DataFrame(data)
 
-    data_set = df.to_json(f"{src_ouptut}revele{annee}.json")
+    # data_set = df.to_json(f"{src_ouptut}revele{annee}.json")
+
+    data_set = df.to_json(orient='records')
+    # print(data_set)
+
+    with open(f"{src_ouptut}revele{annee}.json", 'w') as f:
+        f.write(data_set)
