2d75b99e3b2e8ddfb1bb81b0b85d637bbc347d37
Ertinox-08 <80890993+Ertinox-08@users.noreply.github.com>
2022-01-14 21:00:02 +0100

    Update .py

--- releve.py
+++ releve.py
@@ -6,16 +6,16 @@ src_ouptut = "data/output/"
 
 YEAR = ["2015","2016","2017","2018"]
 
+#   annee = 0 / 2015 | annee = 3 / 2018 |
 for annee in YEAR:
     src = f"data/input/releve{annee}.csv"
 
-    data = pd.read_csv(src)
-    df = pd.DataFrame(data)
+    # Lecture des fichiers dans un DataFrame
+    df = pd.read_csv(src)
 
-    # data_set = df.to_json(f"{src_ouptut}revele{annee}.json")
-
+    # Conversion du CSV en JSON
     data_set = df.to_json(orient='index')
-    # print(data_set)
 
+    # Ecriture des fichiers avec les données converti en JSON dans /data/output/...csv
     with open(f"{src_ouptut}revele{annee}.json", 'w') as f:
         f.write(data_set)
