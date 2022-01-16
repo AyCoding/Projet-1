c17c9273edf68b2e06fa316c4148bcb365bf1cc2
Ertinox-08 <80890993+Ertinox-08@users.noreply.github.com>
2022-01-16 20:39:28 +0100

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -9,18 +9,12 @@ const myChart = new Chart(ctx, {
             label: 'Relevés Pluviométriques',
             data: [99.5, 99.0, 95.5, 23.5, 66.5, 92.0, 102.0, 100.5, 164.0, 71.0, 159.0, 194.0],
             fill: true,
-            borderWidth: 2,
-            backgroundColor: ['#333333'],
+            borderWidth: 3,
+            backgroundColor: ['#333'],
+            borderColor: '#ff8000',
             tension: 0.1
         }]
     },
-    options: {
-        scales: {
-            y: {
-                // beginAtZero: true
-            }
-        }
-    }
 });
 
 const MONTHS = ["JANVIER", "FEVRIER", "MARS", "AVRIL", "MAI", "JUIN", "JUILLET", "AOUT", "SEPTEMBRE", "OCTOBRE", "NOVEMBRE",
